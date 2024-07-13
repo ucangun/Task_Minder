fd81a4565d43c5feeda03d19b5911cdee0364900
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-13 16:59:00 +0200

    "Updated App.css with new styles and added functionality to TodoList component in TodoList.jsx"

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
+
 const TodoList = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleClick = () => {
+    const newTodo = {
+      id: Date.now(),
+      text: todo,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
   return (
     <div className="todoList">
-      <input type="text" name="" id="" />
-      <button>+</button>
+      <div className="todoList-input">
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button onClick={handleClick}>+</button>
+      </div>
+      <div className="todoList-container">
+        {todos.map(({ id, text, completed }) => (
+          <div className="todo" key={id}>
+            <p>{text}</p>
+            <div className="resultBox">
+              <i className="fa-solid fa-check"></i>
+              <i className="fa-solid fa-trash"></i>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
